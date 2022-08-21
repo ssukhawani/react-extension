9b89ac12c6b984ebbc5c3bfe117294c29988e530
Sahil Sukhawani <ssukhawani@gmail.com>
2022-08-21 13:48:54 +0530

    webpack setup

---
+++ src/options/options.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client';
+
+
+const test = <h1>Option Page</h1>
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+const root = createRoot(container);
+root.render(test)
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,55 @@
-const path = require('path');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlPlugin = require("html-webpack-plugin");
 
-module.exports={
-    mode:"development",
-    entry:"./src/test.tsx",
-    module:{
-        rules:[
-            {
-                use:'ts-loader',
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-            }
-        ]
+module.exports = {
+  mode: "development",
+  devtool: "cheap-module-source-map",
+  entry: {
+    popup: path.resolve("src/popup/popup.tsx"),
+    options: path.resolve("src/options/options.tsx"),
+  },
+  module: {
+    rules: [
+      {
+        use: "ts-loader",
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve("src/static"),
+          to: path.resolve("dist"),
+        },
+      ],
+    }),
+    ...getHtmlPlugins(["popup", "options"]),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve("dist"),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
     },
-    resolve: {
-        extensions: ['.tsx','.ts','.js'],
-    },
-    output:{
-        filename:"index.js",
-        path:path.resolve(__dirname,"dist")
-    }
+  },
+};
+
+function getHtmlPlugins(chunks) {
+  return chunks.map(
+    (chunk) =>
+      new HtmlPlugin({
+        title: `${chunk} html page`,
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  );
 }

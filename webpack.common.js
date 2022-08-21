eecc3765d4a27f9a8bfa00ad40728a5a7f9f1d59
Sahil Sukhawani <ssukhawani@gmail.com>
2022-08-21 20:36:59 +0530

    webpack dev prod setup

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,14 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const HtmlPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  devtool: "cheap-module-source-map",
   entry: {
     popup: path.resolve("src/popup/popup.tsx"),
     options: path.resolve("src/options/options.tsx"),
+    background: path.resolve("src/background/background.ts"),
+    contentScripts: path.resolve("src/contentScripts/contentScripts.ts"),
   },
   module: {
     rules: [
@@ -21,12 +22,15 @@ module.exports = {
         test: /\.css$/,
       },
       {
-        type:"asset/resource",
+        type: "asset/resource",
         test: /\.(jpg|jpeg|png|svg|woff|woff2|ttf|eot|ico)$/,
-      }
+      },
     ],
   },
   plugins: [
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false,
+    }),
     new CopyPlugin({
       patterns: [
         {
